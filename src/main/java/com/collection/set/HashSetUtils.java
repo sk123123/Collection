0c7536ad2a6dd597e9ea27e9da27b68ed698c0fd
sunkai123 <448105161@qq.com>
2020-02-05 11:18:34 +0800

    HashSet

--- src/main/java/com/collection/set/HashSetUtils.java
+++ src/main/java/com/collection/set/HashSetUtils.java
@@ -1,4 +1,20 @@
 package com.collection.set;
 
+import java.util.HashSet;
+
+/**
+ * hashset分析
+ */
 public class HashSetUtils {
+    public static void main(String[] args) {
+        //实例化 map = new HashMap<>();
+        HashSet hashSet = new HashSet();
+        //key值存放 无序
+        hashSet.add("hashSet");
+        //元素可以null
+        hashSet.add(null);
+        // value 存放 private static final Object PRESENT = new Object();
+        hashSet.add("hashSet");//元素不可重复
+        System.out.println(hashSet);
+    }
 }
